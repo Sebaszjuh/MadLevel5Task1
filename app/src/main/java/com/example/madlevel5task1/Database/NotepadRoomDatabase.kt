43bac30aee9ee339b3ed74059d55a5676311518a
griends1 <Steelpan1>
2020-09-22 13:29:44 +0200

    Added converter & implemented into db.

--- app/src/main/java/com/example/madlevel5task1/Database/NotepadRoomDatabase.kt
+++ app/src/main/java/com/example/madlevel5task1/Database/NotepadRoomDatabase.kt
@@ -4,10 +4,17 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.madlevel5task1.DAO.NoteDao
 import com.example.madlevel5task1.Model.Note
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class NotepadRoomDatabase : RoomDatabase() {
 
     abstract fun noteDao(): NoteDao
@@ -27,6 +34,17 @@ abstract class NotepadRoomDatabase : RoomDatabase() {
                             NotepadRoomDatabase::class.java, DATABASE_NAME
                         )
                             .fallbackToDestructiveMigration()
+                            .addCallback(object : RoomDatabase.Callback() {
+                                override fun onCreate(db: SupportSQLiteDatabase) {
+                                    super.onCreate(db)
+                                    INSTANCE?.let { database ->
+                                        CoroutineScope(Dispatchers.IO).launch {
+                                            database.noteDao().insertNote(Note("Title", Date(), ""))
+                                        }
+                                    }
+                                }
+                            })
+
                             .build()
                     }
                 }
