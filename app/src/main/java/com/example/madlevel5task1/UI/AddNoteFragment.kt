45b0bd3c39e35a6a84fd3ebd3ce355b4b2a27618
griends1 <Steelpan1>
2020-09-22 21:05:18 +0200

    Added fragments

--- app/src/main/java/com/example/madlevel5task1/UI/AddNoteFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/UI/AddNoteFragment.kt
@@ -6,27 +6,62 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.example.madlevel5task1.Model.NoteViewModel
 import com.example.madlevel5task1.R
+import kotlinx.android.synthetic.main.fragment_add_note.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class AddNoteFragment : Fragment() {
 
+    private val viewModel: NoteViewModel by viewModels()
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_note, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        btnSave.setOnClickListener {
+//            saveNote()
         }
+
+        observeNote()
     }
+
+    private fun observeNote() {
+
+        viewModel.note.observe(viewLifecycleOwner, Observer { note ->
+            if (note != null) {
+                tilNoteTitle.editText?.setText(note.title)
+                tilNoteText.editText?.setText(note.text)
+            }
+        })
+
+//        viewModel.error.observe(viewLifecycleOwner, Observer { message ->
+//            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+//        })
+//
+//        viewModel.success.observe(viewLifecycleOwner, Observer { success ->
+//            //"pop" the backstack, this means we destroy this fragment and go back to the RemindersFragment
+//            findNavController().popBackStack()
+//            hideKeyboard()
+//        })
+    }
+
+//    private fun saveNote() {
+//        viewModel.updateNote(
+//            tilNoteTitle.editText?.text.toString(),
+//            tilNoteText.editText?.text.toString()
+//        )
+//    }
 }
--- app/src/main/java/com/example/madlevel5task1/UI/NotepadFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/UI/NotepadFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.madlevel5task1.Model.NoteViewModel
 import com.example.madlevel5task1.R
+import kotlinx.android.synthetic.main.fragment_notepad.*
+import androidx.lifecycle.Observer
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class NotepadFragment : Fragment() {
+
+    private val viewModel: NoteViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +29,19 @@ class NotepadFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        observeAddNoteResult()
 
     }
+
+    private fun observeAddNoteResult() {
+        viewModel.note.observe(viewLifecycleOwner, Observer { note ->
+            note?.let {
+                notepadTitle.text = it.title
+                dateEdited.text = getString(R.string.dateEdited, it.lastUpdated.toString())
+                notepadText.text = it.text
+            }
+        })
+    }
+
+
 }
